07cd2f373d11d42f95fb39840cd8b65d9b961d00
marius <mariuslinklater@gmail.com>
2026-02-26 12:49:41 -0600

    demo for images in js

---
+++ 03-images/sketch.js
@@ -0,0 +1,21 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+let sethKinstleImg;
+
+function preload() {
+  sethKinstleImg = loadImage('sethKinstle.jpg')
+}
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  imageMode(CENTER);
+}
+
+function draw() {
+  background(220);
+  image(sethKinstleImg, mouseX, mouseY, sethKinstleImg.width * 2, sethKinstleImg.height * 2)
+}
