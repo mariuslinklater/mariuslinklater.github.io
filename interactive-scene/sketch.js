626293c0fe7d51dd74979d60f45b300a1e9a700e
marius <mariuslinklater@gmail.com>
2026-03-01 01:32:39 -0600

    added a few images and got movement between aisles done

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -6,11 +6,17 @@
 // - describe what you did to take this project "above and beyond"
 
 let screen; // changes between menu and game and stuff, state variable
+let difficulty = 'easy';
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  screen = 'menu';
+}
 
 let character = {
-  characterX: windowWidth/2,
-  characterY: windowHeight/2,
-  speed: 5
+  characterX: 400,
+  characterY: 400,
+  speed: 12
 };
 
 let characterImage;
@@ -23,11 +29,10 @@ function preload(){
   aisle1Image = loadImage('storeAisle1.jpg');
   aisle2Image = loadImage('storeAisle2.jpg');
   aisle3Image = loadImage('storeAisle3.jpg');
-}
-
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  screen = 'menu';
+  menuImage = loadImage('menu.jpg');
+  easyModeImage = loadImage('menuEasyMode.png');
+  hardModeImage = loadImage('hardMenu.jpg');
+  superHardModeImage = loadImage('superHardMenu.jpg');
 }
 
 function draw() {
@@ -54,7 +59,15 @@ function drawBackground() {
 }
 
 function drawMenu(){
-
+ let diffImage = easyModeImage
+ image(menuImage, 0, 0, windowWidth, windowHeight);
+ image(diffImage, windowWidth/2, windowHeight/1.59, windowWidth/6, windowHeight/7.7)
+ if (mouseIsPressed && mouseX > windowWidth/7 && mouseX < windowWidth/2 && mouseY > windowHeight/3 && mouseY < windowHeight/1.7) {
+    screen = 'aisle1'
+ }
+ if (mouseIsPressed && mouseX > windowWidth/2 && mouseX < windowWidth/1.2 && mouseY > windowHeight/3 && mouseY < windowHeight/1.7){
+  diffImage = hardModeImage
+ }
 }
 
 function introSequence() {
@@ -62,29 +75,47 @@ function introSequence() {
 }
 
 function drawAisle1() {
-
+  image(aisle1Image, 0, 0, windowWidth, windowHeight);
+  if (character.characterX > windowWidth) {
+    screen = "aisle2";
+    character.characterX = 100;
+  }
 }
 
 function drawAisle2() {
+  image(aisle2Image, 0, 0, windowWidth, windowHeight);
+  
+  if (character.characterX > windowWidth) {
+    screen = "aisle3";
+    character.characterX = 100;
+  }
 
+    if (character.characterX < 0) {
+    screen = "aisle1";
+    character.characterX = windowWidth - 100;
+  }
 }
 
 function drawAisle3() {
-
+  image(aisle3Image, 0, 0, windowWidth, windowHeight);
+      if (character.characterX < 0 - 100) {
+    screen = "aisle2";
+    character.characterX = windowWidth - 100;
+    }
 }
 
 function drawCharacter() {
-  if (screen !== 'menu') {
-    image(characterImage, character.characterX, character.characterY, characterImage.width/2, characterImage.height/2);
-    if(keyIsDown(87)) { //w
-      character.characterY -= character.speed;
+  if (screen !== 'menu' && screen !== 'intro') {
+    image(characterImage, character.characterX, character.characterY, 360* (character.characterY/350),  540 * (character.characterY/350) );
+    if(keyIsDown(87) && (character.characterY > windowHeight/2 - 400 * (character.characterY/350))) { //w
+      character.characterY -= character.speed/1.6;
     }
     if(keyIsDown(83)) { //s
-      character.characterY += character.speed;
+      character.characterY += character.speed/1.6;
     }  if(keyIsDown(68)) { //d
       character.characterX += character.speed;
     }  if(keyIsDown(65)) { //a
       character.characterX -= character.speed;
     }
-  }
+  } 
 }
