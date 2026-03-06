ebb6fdf2f53f96ca1ac8acb1d24dfadc5709292a
marius <mariuslinklater@gmail.com>
2026-03-06 14:42:30 -0600

    Update sketch.js

--- array-object/sketch.js
+++ array-object/sketch.js
@@ -1,7 +1,7 @@
 // Arrays and Object Notation
 // Marius Linklater
 // March 5/26
-//
+// most of this stuff is from the interactive scene, but I will show where I put new things of changed old stuff
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
